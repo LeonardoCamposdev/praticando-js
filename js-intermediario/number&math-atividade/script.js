b534b82c3aac0af5d4c80d39defc039d989b1c4a
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-08-06 20:01:33 -0300

    u´p

--- js-intermediario/number&math-atividade/script.js
+++ js-intermediario/number&math-atividade/script.js
@@ -18,3 +18,17 @@ console.log(maiorNumero);
 // Crie uma função para limpar os preços
 // e retornar os números com centavos arredondados
 // depois retorne a soma total
+const listaPrecos = ["R$ 59,99", " R$ 100,222", "R$ 230  ", "r$  200"];
+
+let somaPrecos = 0;
+function limparPrecos(){
+  listaPrecos.forEach((preco) =>{
+    preco = +preco.toUpperCase().replace("R$ ","").replace(",",".").trim();
+    let precoArredondado = Number(preco);
+    +precoArredondado.toFixed(2);
+      somaPrecos += preco;
+    console.log(precoArredondado)
+  })
+}
+limparPrecos();
+console.log(somaPrecos);
