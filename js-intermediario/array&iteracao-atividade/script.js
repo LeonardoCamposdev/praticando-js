06d5191cfe0e8c361bd81d5eb3f8ad9d04664bd1
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-08-07 12:41:10 -0300

    up

--- js-intermediario/array&iteracao-atividade/script.js
+++ js-intermediario/array&iteracao-atividade/script.js
@@ -1,40 +1,41 @@
 // Selecione cada curso e retorne uma array
 // com objetos contendo o título, descricao,
 // aulas e horas de cada curso
-const cursos = document.querySelectorAll(".curso"); // NODELIST
+const cursos = document.querySelectorAll('.curso');
 
-const arrayCursos = Array.from(cursos);  // ARRAY
+const arrayCursos = Array.from(cursos);
 
-const objetosCurso = arrayCursos.map((curso) => {
-  const titulo = curso.querySelector("h1").innerText;
-  const descricao = curso.querySelector("p").innerText;
-  const aulas = curso.querySelector(".aulas").innerText;
-  const horas = curso.querySelector(".horas").innerHTML;
-  return {
-    titulo,
-    descricao,
-    aulas,
-    horas,
-  };
-});
-console.log(objetosCurso);
+const objetoCursos = arrayCursos.map((curso) =>{
+    const titulo = curso.querySelector('h1').innerHTML;
+    const descricao = curso.querySelector('p').innerHTML;
+    const aula =  curso.querySelector('.aulas').innerHTML;
+    const hora =  curso.querySelector('.horas').innerHTML;
+  
+    return{
+      titulo,
+      descricao,
+      aula,
+      hora
+    }
+})
+console.log(objetoCursos);
+
 
 // Retorne uma lista com os
 // números maiores que 100
 const numeros = [3, 44, 333, 23, 122, 322, 33];
-const maioresQue100 = numeros.filter((n) => n > 100);
-
-console.log(maioresQue100);
+const numerosMaiorQue100 = numeros.filter((numero) =>{
+  if(numero > 100){
+    return numero;
+    }
+})
+console.log(numerosMaiorQue100);
 
 // Verifique se Baixo faz parte
 // da lista de instrumentos e retorne true
 const instrumentos = ["Guitarra", "Baixo", "Bateria", "Teclado"];
-
-const instrumentosBaixo = instrumentos.some((instrumento) => {
-  return instrumento === "Baixo";
-});
-
-console.log(instrumentosBaixo);
+const instrumentoBaixo = instrumentos.includes('Baixo');
+console.log(instrumentoBaixo);
 
 // Retorne o valor total das compras
 const compras = [
@@ -60,9 +61,3 @@ const compras = [
   },
 ];
 
-const comprasTotal = compras.reduce((acumulador, compra) => {
-  const precoLimpo = +compra.preco.replace("R$ ", "").replace(",", ".");
-  return acumulador + precoLimpo;
-}, 0);
-
-console.log(comprasTotal);
