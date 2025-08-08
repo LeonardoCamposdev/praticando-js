9d0cd027200c90a39112df6da5deaebc01da98c6
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-08-07 21:06:12 -0300

    upp

--- js-intermediario/function-atividade/script.js
+++ js-intermediario/function-atividade/script.js
@@ -1,13 +1,32 @@
 // Retorne a soma total de caracteres dos
 // parágrafos acima utilizando reduce
-const 
+const paragrafos = document.querySelectorAll("p");
 
+const totalCaracteres = Array.prototype.reduce.call(
+  paragrafos,
+  (acumulador, item) => {
+    return acumulador + item.innerText.length;
+  },
+  0
+);
+console.log(totalCaracteres);
 
 // Crie uma função que retorne novos elementos
 // html, com os seguintes parâmetros
 // tag, classe e conteudo.
+function novosElementos(tag,classe,conteudo){
+  const element = document.createElement(tag);
+  element.classList.add(classe);
+  element.innerHTML = conteudo;
+
+  document.querySelector('section').appendChild(element);
+}
+novosElementos('h2','ativo','Esse aqui pelo menos eu consigo');
 
 
 // Crie uma nova função utilizando a anterior como base
 // essa nova função deverá sempre criar h1 com a
 // classe titulo. Porém o parâmetro conteudo continuará dinâmico
+const novoH1 = novosElementos.bind(null,'h1','titulo')
+
+console.log(novoH1('Vamos de teste, (deu certo hahaha)'));
