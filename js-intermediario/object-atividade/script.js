4caccff15efe54276c7dde1689eaf31c457d454f
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-08-09 12:30:58 -0300

    upp

--- js-intermediario/object-atividade/script.js
+++ js-intermediario/object-atividade/script.js
@@ -1,46 +1,37 @@
 // Crie uma função que verifique
 // corretamente o tipo de dado
-
-function verificarDado(dado) {
-  return Object.prototype.toString.call(dado);
+function verificarDado(dado){
+  const verificar = Object.prototype.toString.call(dado);
+  return verificar;
 }
 
 console.log(verificarDado([]));
 
+
 // Crie um objeto quadrado com
 // a propriedade lados e torne
 // ela imutável
-const quadrado = {};
+const moto = {};
 
-Object.defineProperties(quadrado, {
-  lados: {
-    value: 4,
-    enumerable: true,
+Object.defineProperties(moto,{
+  rodas:{
+    value: 2,
+    writable: false,
   },
-});
-console.log(quadrado);
+
+  cor:{
+    value: 'Vermelha',
+    writable: false,
+  }
+})
+
+moto.cor = 'azul';
+console.log(moto)
+
 // Previna qualquer mudança
 // no objeto abaixo
-const configuracao = {
-  width: 800,
-  height: 600,
-  background: "#333",
-};
 
-console.log(configuracao);
-Object.freeze(configuracao);
-
-console.log(Object.isFrozen(configuracao));
 
 // Liste o nome de todas
 // as propriedades do
 // protótipo de String e Array
-const propiedadesPrototipoString = Object.getOwnPropertyNames(
-  String.prototype
-);
-const propiedadesPrototipoArray = Object.getOwnPropertyNames(
-  Array.prototype
-);
-
-console.log(propiedadesPrototipoString);
-console.log(propiedadesPrototipoArray);
