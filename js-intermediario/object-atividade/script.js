662cd9378f6eec19303779824eb2194f6593cc59
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-08-11 12:26:45 -0300

    uppp

--- js-intermediario/object-atividade/script.js
+++ js-intermediario/object-atividade/script.js
@@ -11,12 +11,36 @@ console.log(verificarDado([]));
 // Crie um objeto quadrado com
 // a propriedade lados e torne
 // ela imutável
+const quadrado = {};
 
+Object.defineProperties(quadrado,{
+  lados:{
+    value: 4,
+      writable:false,
+  }
+
+})
+quadrado.lados = 50;
+console.log(quadrado);
 
 // Previna qualquer mudança
 // no objeto abaixo
+const configuracao = {
+  width: 800,
+  height: 600,
+  background: "#333",
+};
+
+Object.freeze(configuracao);
+configuracao.width = 1000;
+console.log(configuracao);
 
 
 // Liste o nome de todas
 // as propriedades do
 // protótipo de String e Array
+const propiedadesString = Object.getOwnPropertyNames(String.prototype);
+const propiedadesArray = Object.getOwnPropertyNames(Array.prototype);
+
+console.log(propiedadesString);
+console.log(propiedadesArray);
--- js-intermediario/prototype-atividade/script.js
+++ js-intermediario/prototype-atividade/script.js
@@ -2,36 +2,32 @@
 // Deve conter nome, sobrenome e idade
 // Crie um método no protótipo que retorne
 // o nome completo da pessoa
-function Pessoas(nomeAtribuido,sobreAtribuido,idadeAtribuida){
-  this.nome = nomeAtribuido;
-  this.sobrenome = sobreAtribuido;
-  this.idade = idadeAtribuida;
+function Pessoa(nome,sobrenome,idade){
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.idade = idade;
 }
 
-Pessoas.prototype.nomeCompleto = function(){
+Pessoa.prototype.nomeCompleto = function(){
   return this.nome + this.sobrenome;
 }
 
-const leo = new Pessoas('Leonardo', ' Campos', 24);
-const ravi = new Pessoas('Ravi Mayk ', 'Muniz', 5);
+const leonardo = new Pessoa('Leonardo',' Campos', 24);
+const ravi = new Pessoa('Ravi', ' Campos', 5);
+console.log(leonardo)
+console.log(leonardo.nomeCompleto());
+console.log(ravi);
+console.log(ravi.nomeCompleto());
 
 // Liste os métodos acessados por 
 // dados criados com NodeList,
 // HTMLCollection, Document
-console.log(NodeList.prototype);
-console.log(HTMLAllCollection.prototype);
-console.log(Document.prototype);
+console.log(Object.getOwnPropertyNames(NodeList.prototype));
+console.log(Object.getOwnPropertyNames(HTMLAllCollection.prototype));
+console.log(Object.getOwnPropertyNames(Document.prototype));
 
 // Liste os construtores dos dados abaixo
-const li = document.querySelector('li');
 
-li; // HTMLLIElement
-li.click; // Function
-li.innerText; // String
-li.value; // Number
-li.hidden; // Boolean
-li.offsetLeft; // Number
-li.click(); // undefined
 
 // Qual o construtor do dado abaixo:
-li.hidden.constructor.name; // String
+li.hidden.constructor.name;
