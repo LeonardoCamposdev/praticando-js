d0dd9772c664a96926702c22a2e71b64c3325a38
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-08-19 08:38:17 -0300

    upp

--- efeitos-no-dom/tooltip/js/modules/tooltip.js
+++ efeitos-no-dom/tooltip/js/modules/tooltip.js
@@ -1,42 +1,34 @@
 export default function initTooltip() {
-  const tooltips = document.querySelectorAll("[data-tooltip]");
-
-  function onMouseOver() {
-    const tooltipBox = criartoolTipBox(this);
-
-    onMouseMove.tooltipBox = tooltipBox;
-    this.addEventListener("mousemove", onMouseMove);
-
-    onMouseLeave.tooltipBox = tooltipBox;
-    onMouseLeave.element = this;
-    this.addEventListener("mouseleave", onMouseLeave);
-  }
-
-  const onMouseLeave = {
-    handleEvent() {
-      this.tooltipBox.remove();
-      this.element.removeEventListener("mousemove", onMouseMove);
-      this.element.removeEventListener("mouseleave", onMouseLeave);
-    },
-  };
-
-  const onMouseMove = {
-    handleEvent(event) {
-      this.tooltipBox.style.top = event.pageY + 20 + "px";
-      this.tooltipBox.style.left = event.pageX + 20 + "px";
-    },
-  };
-
-  function criartoolTipBox(element) {
-    const toolTipBox = document.createElement("div");
-    const text = element.getAttribute("aria-label");
-    toolTipBox.classList.add("tooltip");
-    toolTipBox.innerText = text;
-    document.body.appendChild(toolTipBox);
-    return toolTipBox;
-  }
-
-  tooltips.forEach((item) => {
-    item.addEventListener("mouseover", onMouseOver);
-  });
 }
+
+const tooltips = document.querySelectorAll('[data-tooltip]');
+
+function onMouseOver(){
+  const tooltipBox = criarTooltipBox(this);
+
+  onMouseMove.tooltipBox = tooltipBox;
+  this.addEventListener('mousemove', onMouseMove);
+}
+
+const onMouseMove = {
+  handleEvent(event){
+    this.tooltipBox.style.top = event.pageY + 20 + 'px';
+    this.tooltipBox.style.left = event.pageX + 20 + 'px';
+  }
+}
+
+
+
+function criarTooltipBox(element){
+  const tooltipBox = document.createElement('div');
+  const text = element.getAttribute('aria-label');
+  tooltipBox.classList.add('tooltip');
+  tooltipBox.innerText = text;
+  document.body.appendChild(tooltipBox);
+  return tooltipBox;
+}
+
+
+tooltips.forEach((item) =>{
+  item.addEventListener('mouseover', onMouseOver);
+})
