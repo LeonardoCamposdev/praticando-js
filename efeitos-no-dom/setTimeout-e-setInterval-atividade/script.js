8163ab061967aaeb8ccee3bc1deaddb6cdb53ead
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-08-20 19:56:55 -0300

    uppp

--- efeitos-no-dom/setTimeout-e-setInterval-atividade/script.js
+++ efeitos-no-dom/setTimeout-e-setInterval-atividade/script.js
@@ -1,35 +1,23 @@
 // Mude a cor da tela para azul e depois para vermelho a cada 2s.
-setInterval(()=>{
-  document.body.classList.toggle("active");
-},2000);
+//const body = document.querySelector("body");
+
+//const ChangeTela = setInterval(() => {
+  //body.classList.toggle("active");
+//}, 2000);
 
 // Crie um cronometro utilizando o setInterval. Deve ser possível
 // iniciar, pausar e resetar (duplo clique no pausar).
-const iniciar = document.querySelector(".iniciar"); //BTN
-const pausar = document.querySelector(".pausar"); //BTN
-const tempo = document.querySelector(".tempo"); //SPAN
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const tempo = document.querySelector('.tempo')
 
-iniciar.addEventListener("click",iniciarTempo);
-pausar.addEventListener("click",pausarTempo);
-pausar.addEventListener("dblclick",resetarTempo);
-
-let i = 0; //CRONOMETRO
-let timer; //SEM VALOR DEFINIDO
-
-function iniciarTempo(){
-  timer = setInterval(() =>{
-    tempo.innerText = i++;
-  },100)
-
-  iniciar.setAttribute("disabled",'');
-}
-
-function pausarTempo(){
-  clearInterval(timer);
-  iniciar.removeAttribute("disabled");
-}
-
-function resetarTempo(){
+let  i = 0;
+function startCronometro(){
   tempo.innerText = 0;
-  i = 0;
 }
+
+function teste(){
+  console.log('teste')
+}
+
+iniciar.addEventListener('click',startCronometro);
