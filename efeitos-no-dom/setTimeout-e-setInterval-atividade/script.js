6faf28a3c8fb9ecf670a88dddf564f59b83cb530
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-08-20 23:48:48 -0300

    up

--- efeitos-no-dom/setTimeout-e-setInterval-atividade/script.js
+++ efeitos-no-dom/setTimeout-e-setInterval-atividade/script.js
@@ -2,39 +2,37 @@
 //const body = document.querySelector("body");
 
 //const ChangeTela = setInterval(() => {
-  //body.classList.toggle("active");
+//body.classList.toggle("active");
 //}, 2000);
 
 // Crie um cronometro utilizando o setInterval. Deve ser possível
 // iniciar, pausar e resetar (duplo clique no pausar).
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const tempo = document.querySelector('.tempo')
-  pausar.disabled = true;
+const iniciar = document.querySelector(".iniciar");
+const pausar = document.querySelector(".pausar");
+const tempo = document.querySelector(".tempo");
+pausar.disabled = true;
 
-let  i = 0;
+let i = 0;
 let timer;
-function startCronometro(){
-  timer = setInterval(()=>{
+function startCronometro() {
+  timer = setInterval(() => {
     tempo.innerText = i++;
-  },100)
+  }, 100);
   iniciar.disabled = true;
-   pausar.disabled = false;
+  pausar.disabled = false;
 }
 
-function pauseCronometro(){
+function pauseCronometro() {
   clearInterval(timer);
   iniciar.disabled = false;
-
 }
 
-function cleanCronometro(){
+function cleanCronometro() {
   tempo.innerText = 0;
   i = i.innerText = 0;
-   pausar.disabled = true;
+  pausar.disabled = true;
 }
 
-
-iniciar.addEventListener('click',startCronometro);
-pausar.addEventListener('click',pauseCronometro);
-pausar.addEventListener('dblclick',cleanCronometro);
+iniciar.addEventListener("click", startCronometro);
+pausar.addEventListener("click", pauseCronometro);
+pausar.addEventListener("dblclick", cleanCronometro);
