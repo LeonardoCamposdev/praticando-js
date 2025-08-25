d770648b5dec5fe2127117da565b3ab288aa8920
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-08-25 17:20:53 -0300

    upp

--- efeitos-no-dom/date-object/js/modules/funcionamento.js
+++ efeitos-no-dom/date-object/js/modules/funcionamento.js
@@ -1,17 +1,21 @@
 export default function initFuncionamento(){
 
 }
+
 const funcionamento = document.querySelector('[data-semana]');
-const funcionamentoSemana = funcionamento.dataset.semana.split(',').map(Number);
-const funcionamentoHorario = funcionamento.dataset.horario.split(',').map(Number);
+const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
+const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+
 
 const dataAgora = new Date();
-const diaHoje = dataAgora.getDay();
-const horaHoje = dataAgora.getHours();
+const diaAgora = dataAgora.getDay();
+const horarioAgora = dataAgora.getHours();
 
-const semanaAberto = funcionamentoSemana.includes(diaHoje)!== -1;
-const horarioAberto = (horaHoje >= funcionamentoHorario[0] && horaHoje < funcionamentoHorario[1]);
+
+
+const semanaAberto = diasSemana.indexOf(diaAgora)!== -1;
+const horarioAberto = (horarioAgora >= horarioSemana[0] &&  horarioAgora < horarioSemana[1]);
 
 if(semanaAberto && horarioAberto){
-  funcionamento.classList.add('aberto');
+  funcionamento.classList.add('aberto')
 }
