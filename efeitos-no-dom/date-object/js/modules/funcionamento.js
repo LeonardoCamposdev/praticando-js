a12e60ef67e12273527fb2bba7e382de068de79b
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-08-26 22:42:20 -0300

    upp

--- efeitos-no-dom/date-object/js/modules/funcionamento.js
+++ efeitos-no-dom/date-object/js/modules/funcionamento.js
@@ -1,21 +1,17 @@
-export default function initFuncionamento(){
+export default function initFuncionamento() {
+  const funcionamento = document.querySelector("[data-semana]");
+  const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+  const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
 
-}
+  const dataHoje = new Date();
+  const diaHoje = dataHoje.getDay();
+  const horarioHoje = dataHoje.getHours();
 
-const funcionamento = document.querySelector('[data-semana]');
-const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
-const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+  const semanaAberto = diasSemana.indexOf(diaHoje) !== -1;
+  const horarioAberto =
+    horarioHoje >= horarioSemana[0] && horarioHoje < horarioSemana[1];
 
-
-const dataAgora = new Date();
-const diaAgora = dataAgora.getDay();
-const horarioAgora = dataAgora.getHours();
-
-
-
-const semanaAberto = diasSemana.indexOf(diaAgora)!== -1;
-const horarioAberto = (horarioAgora >= horarioSemana[0] &&  horarioAgora < horarioSemana[1]);
-
-if(semanaAberto && horarioAberto){
-  funcionamento.classList.add('aberto')
+  if (semanaAberto && horarioAberto) {
+    funcionamento.classList.add("aberto");
+  }
 }
