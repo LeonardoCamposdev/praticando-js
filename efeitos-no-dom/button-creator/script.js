eb2f935ac78693cc2ac6aa84613ac94876c6f9ae
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-08-27 12:31:46 -0300

    upp

--- efeitos-no-dom/button-creator/script.js
+++ efeitos-no-dom/button-creator/script.js
@@ -1,65 +1,51 @@
-const form = document.getElementById("controles");
-const btn = document.querySelector(".button");
-const cssText = document.querySelector(".css");
-form.addEventListener("change", handleChange);
+const form = document.getElementById('controles');
+const btn = document.querySelector('.button');
+const cssText = document.querySelector('.css');
 
 const handleStyle = {
   element: btn,
-  texto(value) {
+  texto(value){
     this.element.innerText = value;
   },
-  color(value) {
+
+  color(value){
     this.element.style.color = value;
   },
-  backgroundColor(value) {
+
+  backgroundColor(value){
     this.element.style.backgroundColor = value;
   },
-  height(value) {
-    this.element.style.height = value + "px";
+
+  height(value){
+    this.element.style.height = value + 'px';
   },
-  width(value) {
-    this.element.style.width = value + "px";
+
+  width(value){
+    this.element.style.width = value + 'px';
   },
-  border(value) {
+
+  border(value){
     this.element.style.border = value;
   },
-  borderRadius(value) {
-    this.element.style.borderRadius = value + "px";
+
+  borderRadius(value){
+    this.element.style.borderRadius = value + 'px';
   },
-  fontFamily(value) {
+
+  fontFamily(value){
     this.element.style.fontFamily = value;
   },
-  fontSize(value) {
-    this.element.style.fontSize = value + "rem";
-  },
-};
 
-function handleChange(event) {
+  fontSize(value){
+    this.element.style.fontSize = value + 'rem';
+  }
+}
+
+function handleChange(event){
   const name = event.target.name;
   const value = event.target.value;
 
   handleStyle[name](value);
-  saveValue(name,value);
-  showCss();
 }
 
-function saveValue(name,value){
-  localStorage[name] = value;
-}
-
-function setValue(){
-  const properties = Object.keys(localStorage);
-  properties.forEach((propertie) =>{
-    form.elements[propertie].value = localStorage[propertie];
-    handleStyle[propertie](localStorage[propertie]);
-  });
-}
-setValue();
-
-
-function showCss() {
-  cssText.innerHTML =
-    "<span>" + btn.style.cssText.split("; ").join(";</span><span>");
-}
-
-
+form.addEventListener('change',handleChange);
