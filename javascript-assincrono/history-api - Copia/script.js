a639fa96eed1af4ff6791d2bec4a89e8c6a78639
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-09-03 14:34:52 -0300

    Update script.js

--- javascript-assincrono/history-api - Copia/script.js
+++ javascript-assincrono/history-api - Copia/script.js
@@ -2,29 +2,11 @@ const links = document.querySelectorAll('a');
 
 function handleClick(event){
   event.preventDefault();
-  const href = event.target.href;
-  pageFetch(href);
+  const url = event.target.href;
 }
 
-async function pageFetch(url){
-  const responsePage = await fetch(url);
-  const pageText = await responsePage.text();
-  changePage(pageText);
-}
-
-function changePage(newText){
-  const newHtml = document.createElement('div');
-  newHtml.innerHTML = newText;
-  console.log(newHtml);
-
-  const oldContent = document.querySelector('.content');
-  const newContent = newHtml.querySelector('.content');
-
-  oldContent.innerHTML = newContent.innerHTML;
-  document.title = newHtml.querySelector('title').innerText;
-}
 
 
 links.forEach((link) =>{
-  link.addEventListener('click',handleClick);
+  link.addEventListener('click', handleClick);
 })
