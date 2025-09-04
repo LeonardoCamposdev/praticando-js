3323e0fde9bdd455a6a8ca2bff71812a0d052956
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-09-04 12:42:02 -0300

    up

--- efeitos-no-dom/animais-fetch/js/modules/fetch-animais.js
+++ efeitos-no-dom/animais-fetch/js/modules/fetch-animais.js
@@ -1,25 +1,22 @@
-import initAnimaNumeros from "./anima-numeros.js";
+export default function initFetchAnimais(){
 
-export default function initFetchAnimais() {
-  async function fetchAnimais(url) {
-    const animaisResponse = await fetch(url);
-    const animaisJSON = await animaisResponse.json();
-    const numerosGrid = document.querySelector(".numeros-grid");
-    animaisJSON.forEach((animal) => {
-      const createAnimal = documentAnimais(animal);
-      numerosGrid.appendChild(createAnimal);
-    });
-    initAnimaNumeros();
-  }
-  
-  function documentAnimais(animal) {
-    const div = document.createElement("div");
-    div.classList.add("numero-animal");
-    div.innerHTML = `<h3>${animal.especie}</h3><span data-numero> ${animal.total}</span>`;
-    return div;
-  }
-  
-  fetchAnimais("./animaisapi.json");
-  
 }
 
+async function fetchAnimais(animais){
+  const responseAnimais = await fetch(animais);
+  const animaisJSON = await responseAnimais.json();
+  animaisJSON.forEach((animal)=>{
+    animaisDom(animal)
+  })
+}
+
+function animaisDom(animal){
+  const div = document.createElement('div');
+  div.classList.add('numero-animal');
+  div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`; 
+  const numerosGrid = document.querySelector('.numeros-grid');
+  numerosGrid.appendChild(div);
+  return div;
+}
+
+fetchAnimais('./animaisapi.json');
