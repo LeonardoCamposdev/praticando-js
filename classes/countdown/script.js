8fade6f6ad4d99dbd95811ca60a6c940a605ed7f
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-09-10 20:25:21 -0300

    upp

--- classes/countdown/script.js
+++ classes/countdown/script.js
@@ -2,3 +2,11 @@ import Countdown from "./countdown.js";
 
 const tempoParaAniversario = new Countdown("10 February 2026 23:59:59 GMT-0300");
 console.log(tempoParaAniversario.total);
+
+async function AniversarioDom(){
+  const div = document.querySelector('.div');
+  div.innerText = JSON.stringify(tempoParaAniversario.total);
+}
+
+setInterval(AniversarioDom, 1000);
+AniversarioDom();
